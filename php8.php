a4fbf259444edf5fd5f787153b1ffee53c600c32
PAOPLAKEM <94538655+PAOPLAKEM@users.noreply.github.com>
2022-10-09 17:49:51 +0700

    6304062630253

---
+++ php8.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<form action="insert.php" method="post">
+    username<input type="text" name="username"><br>
+ name: <input type="text" name="name"><br>
+<br>
+password:<input type="password" name="password"><br>
+address:<textarea name="address" rows="3" cols="40"></textarea><br>
+moblie:<input type="text" name="mobile">   <br>
+email: <input type="text" name="email"><br>
+<input type="submit" value="เพิ่ม">
+</form>
+</body>
+</html>
